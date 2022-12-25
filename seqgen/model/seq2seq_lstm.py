c46a4a5ffb290f4bea11802df72a279a47311f76
tnodecode <s2.tf@we-code-it.de>
2022-12-25 12:54:26 +0100

    Training of Seq2Seq works

--- seqgen/model/seq2seq_lstm.py
+++ seqgen/model/seq2seq_lstm.py
@@ -3,41 +3,60 @@ import torch.nn.functional as F
 
 
 class EncoderRNN(torch.nn.Module):
-    def __init__(self, vocab_size, embedding_dim, hidden_size, num_layers=1, device='cpu'):
+    def __init__(self, vocab_size, embedding_dim, hidden_size, num_layers=3, dropout=0.2, bidirectional=True, device='cpu'):
         super(EncoderRNN, self).__init__()
         self.device = device
         self.hidden_size = hidden_size
         self.num_layers = num_layers
+        self.bidirectional = bidirectional
 
         self.embedding = torch.nn.Embedding(vocab_size, embedding_dim)
-        self.lstm = torch.nn.LSTM(embedding_dim, hidden_size, batch_first=True)
+        self.dropout = torch.nn.Dropout(dropout)
+        self.lstm = torch.nn.LSTM(embedding_dim + 4, hidden_size, num_layers=num_layers, dropout=dropout, bidirectional=bidirectional, batch_first=True)
 
-    def forward(self, input, hidden):
-        embedded = self.embedding(input)
-        output, hidden = self.lstm(embedded, hidden)
+    def forward(self, x, coordinates, hidden):
+        # First run the input sequences through an embedding layer
+        x = self.dropout(self.embedding(x))
+        # Next pass the embeddings to an activation function
+        x = F.relu(x)
+        # Concatenate embeddings with coordinates
+        x = torch.cat([x, coordinates.unsqueeze(dim=1)], dim=2)
+        # Now we need to run the embeddings through the LSTM layer
+        output, hidden = self.lstm(x, hidden)
         return output, hidden
 
     def initHidden(self, batch_size, device='cpu'):
-        return torch.zeros(self.num_layers, batch_size, self.hidden_size).to(device)
+        if self.bidirectional:
+            num_layers = 2 * self.num_layers
+        else:
+            num_layers = self.num_layers
+        return torch.zeros(num_layers, batch_size, self.hidden_size).to(device)
 
 
 class DecoderRNN(torch.nn.Module):
-    def __init__(self, hidden_size, vocab_size, num_layers=1, device='cpu'):
+    def __init__(self, embedding_dim, hidden_size, vocab_size, num_layers=3, dropout=0.2, bidirectional=True, device='cpu'):
         super(DecoderRNN, self).__init__()
         self.device = device
         self.hidden_size = hidden_size
         self.num_layers = num_layers
+        self.bidirectional = bidirectional
 
-        self.embedding = torch.nn.Embedding(vocab_size, hidden_size)
-        self.lstm = torch.nn.LSTM(hidden_size, hidden_size, batch_first=True)
-        self.fc = torch.nn.Linear(hidden_size, vocab_size)
+        self.dropout = torch.nn.Dropout(dropout)
+        self.embedding = torch.nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = torch.nn.LSTM(embedding_dim + 0, hidden_size, num_layers=num_layers, dropout=dropout, bidirectional=bidirectional, batch_first=True)
+        if self.bidirectional:
+            self.fc = torch.nn.Linear(2*hidden_size, vocab_size)
+        else:
+            self.fc = torch.nn.Linear(hidden_size, vocab_size)
         self.softmax = torch.nn.LogSoftmax(dim=2)
 
-    def forward(self, input, hidden):
+    def forward(self, x, coordinates, hidden):
         # First run the input sequences through an embedding layer
-        x = self.embedding(input)
+        x = self.dropout(self.embedding(x))
         # Next pass the embeddings to an activation function
         x = F.relu(x)
+        # Concatenate embeddings with coordinates
+        #x = torch.cat([x, coordinates.unsqueeze(dim=1)], dim=2)
         # Now we need to run the embeddings through the LSTM layer
         x, hidden = self.lstm(x, hidden)
         # Next run tensor through a fully connected layer that maps the LSTM outputs to the predicted classes
@@ -47,5 +66,9 @@ class DecoderRNN(torch.nn.Module):
         # Return the prediction and the hidden state of the decoder
         return x, hidden
 
-    def initHidden(self, batch_size):
-        return torch.zeros(self.num_layers, batch_size, self.hidden_size).to(self.device)
+    def initHidden(self, batch_size, device='cpu'):
+        if self.bidirectional:
+            num_layers = 2 * self.num_layers
+        else:
+            num_layers = self.num_layers
+        return torch.zeros(num_layers, batch_size, self.hidden_size).to(device)
--- seqgen/seq_gen.py
+++ seqgen/seq_gen.py
@@ -19,8 +19,39 @@ tmpl_in = ["sum", "LETTER", "=", "DIGIT", "LETTER"]
 tmpl_out = ["\\sum", "\\limits", "_",
             "{", "LETTER", "=", "DIGIT", "}", "^", "LETTER"]
 
+def gen_default_box(x0, y0, box_height, box_width, seq_in, seq_out, idx, in_voc, out_voc):
+    _box_height = r.uniform(0.9, 1.1) * box_height
+    _box_width = r.uniform(0.9, 1.1) * box_height
+    _x0 = r.uniform(0.9, 1.1) * x0
+    _y0 = r.uniform(0.9, 1.1) * y0
+    # Append tokens to input and output sequence
+    seq_in.append([in_voc[idx], _x0, _y0, _x0 + _box_height, _y0 + _box_width])
+    seq_out.append(out_voc[idx])
+    return seq_in, seq_out, _box_width
 
-def generate_random_sequence(in_voc: list, out_voc: list, continue_prob=0.9, max_length=20, padding=True):
+def gen_exponent_box(x0, y0, box_height, box_width, seq_in, seq_out, idx, in_voc, out_voc):
+    _box_height = r.uniform(0.4, 0.6) * box_height
+    _box_width = r.uniform(0.9, 1.1) * box_height
+    _x0 = r.uniform(0.7, 1.1) * x0
+    _y0 = r.uniform(0.0, 0.8) * y0
+    # Append tokens to input and output sequence
+    seq_in.append([in_voc[idx], _x0, _y0, _x0 + _box_height, _y0 + _box_width])
+    seq_out.append('^')
+    seq_out.append(out_voc[idx])
+    return seq_in, seq_out, _box_width
+
+def gen_index_box(x0, y0, box_height, box_width, seq_in, seq_out, idx, in_voc, out_voc):
+    _box_height = r.uniform(0.4, 0.6) * box_height
+    _box_width = r.uniform(0.9, 1.1) * box_height
+    _x0 = r.uniform(0.7, 1.1) * x0
+    _y0 = r.uniform(1.3, 2.0) * y0
+    # Append tokens to input and output sequence
+    seq_in.append([in_voc[idx], _x0, _y0, _x0 + _box_height, _y0 + _box_width])
+    seq_out.append('_')
+    seq_out.append(out_voc[idx])
+    return seq_in, seq_out, _box_width
+
+def generate_random_sequence(in_voc: list, out_voc: list, continue_prob=0.9, max_length=20, padding=True, swap_prob=0.5, swap_times=5):
     """
     Generate a random training sample for a sequence-to-sequence model
 
@@ -30,8 +61,6 @@ def generate_random_sequence(in_voc: list, out_voc: list, continue_prob=0.9, max
     continue_prob: Probability that one more token will be appended to the sample sequence
     max_length: Maximum length of the sequence
     """
-    assert len(in_voc) == len(
-        out_voc), "Input and output vocabluary must be of the same size"
     seq_in = [['<start>', 0, 0, 0, 0]]
     seq_out = ['<start>']
 
@@ -43,27 +72,36 @@ def generate_random_sequence(in_voc: list, out_voc: list, continue_prob=0.9, max
     while True:
         # Select a random token from the vocabulary
         random_index = r.randint(0, len(in_voc) - 1)
-        # Add random noise to coordinates (makes boxes smaller / larger, shift boxes to the left / right)
-        _box_height = r.uniform(0.9, 1.1) * box_height
-        _box_width = r.uniform(0.9, 1.1) * box_height
-        _x0 = r.uniform(0.9, 1.1) * x0
-        _y0 = r.uniform(0.9, 1.1) * y0
-        # Append tokens to input and output sequence
-        seq_in.append([in_voc[random_index], _x0, _y0,
-                      _x0 + _box_height, _y0 + _box_width])
-        seq_out.append(out_voc[random_index])
-        # Calculate x0 of the next token's bounding box (next token will be placed to the right of the current bounding box)
-        x0 = x0 + 1.1 * box_width
+        random_choice = r.random()
+        
+        # Generate random boxes    
+        if random_choice < 0.8:
+            gen = gen_default_box
+        elif random_choice < 0.9 and len(seq_out) < max_length - 2:
+            gen = gen_index_box
+        elif len(seq_out) < max_length - 2:
+            gen = gen_exponent_box
+        else:
+            gen = gen_default_box
+        
+        seq_in, seq_out, width = gen(x0, y0, box_height, box_width, seq_in, seq_out, random_index, in_voc, out_voc)
+        x0 = x0 + 1.1 * width
 
         # Check if the sequence should be continued
-        if r.random() > continue_prob or len(seq_in) >= (max_length - 1):
+        if r.random() > continue_prob or len(seq_in) >= (max_length - 1) or len(seq_out) >= (max_length - 1):
             break
 
     # Pad sequence with zeros
     if padding:
         for i in range(max_length - len(seq_in)):
             seq_in.append(['<end>', 0, 0, 0, 0])
+        for i in range(max_length - len(seq_out)):
             seq_out.append('<end>')
+            
+    # Randomly swap positions
+    for i in range(swap_times):
+        if r.random() < swap_prob:
+            seq_in = random_swap(seq_in)
 
     return seq_in, seq_out
 
@@ -89,9 +127,30 @@ def save_as_json(samples, filename="samples.json"):
     """
     with open(filename, "w") as fp:
         json.dump(samples, fp, indent=2)
+        
+        
+def random_swap(lst, i=None):
+    """
+    Swap the positions of two elements randomly.
+    If you pass a list like ['a','b','c'] to this function you may get ['b','a','c'] back
+    """
+    if i is None:
+        i = r.randint(1, len(lst) - 2)
+    j = i + 1
+    
+    # Tensors and lists have to treated differently, because tensors are implemented in C
+    # and use pointers, which leads to a different behaviour than lists
+    if type(lst) == list:
+        lst[i], lst[j] = lst[j], lst[i]
+    elif type(lst) == torch.Tensor:
+        lst = torch.clone(lst)
+        vi = torch.clone(lst[i])
+        vj = torch.clone(lst[j])
+        lst[i] = vj
+        lst[j] = vi
+    return lst
 
-
-def generator(num_samples=5, max_length=10):
+def generator(num_samples=5, max_length=10, continue_prob=0.95, swap_prob=0.5, swap_times=5):
     """
     Generate synthetic training samples for a sequence-to-sequence model.
     A training sample consists of an input sequence (feature sequence) and the desired output sequence (target sequence).
@@ -106,18 +165,20 @@ def generator(num_samples=5, max_length=10):
         feature_seq, target_seq = generate_random_sequence(
             in_voc=digits_in+operators_in,
             out_voc=digits_out+operators_out,
-            continue_prob=0.95,
-            max_length=max_length
+            continue_prob=continue_prob,
+            max_length=max_length,
+            swap_prob=swap_prob,
+            swap_times=swap_times
         )
         inputs.append(feature_seq)
         outputs.append(target_seq)
     return inputs, outputs
 
 
-def generate_synthetic_training_data(num_samples=16, max_length=10, device='cpu'):
+def generate_synthetic_training_data(num_samples=16, max_length=10, continue_prob=0.95, swap_prob=0.5, swap_times=5, device='cpu'):
     vocab_in = Vocabulary(vocab_filename="seqgen/vocab_in.txt")
     vocab_out = Vocabulary(vocab_filename="seqgen/vocab_out.txt")
-    features, targets = generator(num_samples, max_length=max_length)
+    features, targets = generator(num_samples, max_length=max_length, continue_prob=continue_prob, swap_prob=swap_prob, swap_times=swap_times)
     features = encode_classes_of_bboxes(features, vocab_in)
     features = normalize_coordinates(features)
     targets = encode_latex_tokens(targets, vocab_out)
