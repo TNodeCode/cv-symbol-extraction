1948d6c3e35781f233d52ca5ad020ae6992b3a18
Sebastian Hönig <hoenigsebastian@gmail.com>
2023-01-09 13:54:39 +0100

    yolov7 prep

--- dataset-generation/datagenerator.py
+++ dataset-generation/datagenerator.py
@@ -1,58 +1,70 @@
 import helper
-import os
 import cv2
 import random
 import torch
 import sys
 from pathlib import Path
-import numpy as np
+from tqdm import tqdm
 
 
-def create_dataset_random(dataset_num, img_num):
-    dataset_path = Path("archive") / f"dataset{dataset_num}"
+def create_dataset_random(dataset_size, number_of_formulas):
+    dataset_path = Path("archive") / f"dataset{dataset_size}"
     if not dataset_path.exists():
         dataset_path.mkdir()
 
+    images_path = Path("archive") / f"dataset{dataset_size}" / "images"
+    if not images_path.exists():
+        images_path.mkdir()
+
+    labels_path = Path("archive") / f"dataset{dataset_size}" / "labels"
+    if not labels_path.exists():
+        labels_path.mkdir()
+
     background_path = Path("archive") / "background_images"
-    images_path = Path("archive") / "extracted_images"
     formulas_path = Path("archive") / "formulas"
 
-    labels = [f for f in os.listdir(images_path) if not f.startswith('.')]
     backgrounds = [cv2.imread(str(file)) for file in background_path.glob("*.jpg")]
-    formulas = np.array([cv2.imread(str(file)) for file in formulas_path.glob("*.png")])
     formulas_label = open(formulas_path / "label.txt", 'r')
     formulas_label = formulas_label.readlines()
 
-
-    filename = 0
-    label_txt = open(dataset_path / "label.txt", 'a')
-
-    while filename < dataset_num:
-        for back in backgrounds:
-            coordinates = torch.zeros((img_num, 4))
-            label_txt.write(str(Path.cwd() / dataset_path / f"{filename}.jpg "))
-            background = back
-            for i in range(img_num):
+    # Label dictionary encoding for Yolov7
+    label_dict = {}
+    num_distinct_labels = 0
+    for j in tqdm(range(dataset_size), total=dataset_size):
+        for background in backgrounds:
+            coordinates = torch.zeros((number_of_formulas, 4))
+            label_txt = open(labels_path / f"{j}.txt", 'a')
+            for i in range(number_of_formulas):
                 formula_label_int = random.randint(0, len(formulas_label)-1)
                 formula_label = formulas_label[formula_label_int].split(' ')
                 image = cv2.imread(formula_label[0])
                 formula_label[-1] = formula_label[-1][:-1]
-                label = list(map(lambda x: x.split(','), formula_label[1:]))
+                labels = list(map(lambda x: x.split(','), formula_label[1:]))
+                for label in labels:
+                    # check if label already in label_dict
+                    if label[-1] not in label_dict:
+                        label_dict[label[-1]] = num_distinct_labels
+                        num_distinct_labels += 1
+                    # Replace all "labels" with their corresponding label_dict value (aka the encoded yolov7 labels)
+                    label[-1] = label_dict[label[-1]]
 
-                background, label_line = helper.place_image_on_background(
-                    label, image, background, coordinates, i)
-                
-                label_txt.write(label_line)
+                background, label_line, flag = helper.place_image_on_background(
+                    labels, image, background, coordinates, i)
 
-            label_txt.write('\n')
+                if not flag:
+                    for line_ in label_line:
+                        # label_line format is [class, x_center, y_center, width, height]
+                        label_txt.write(f"{line_[0]} {line_[1]} {line_[2]} {line_[3]} {line_[4]}")
+                        # new line
+                        label_txt.write("\n")
+            cv2.imwrite(str(images_path / f"{j}.jpg"), background)
+            label_txt.close()
 
-            cv2.imwrite(str(dataset_path / f"{filename}.jpg"), background)
-
-            filename += 1
-            if filename >= dataset_num:
-                break
-
-    label_txt.close()
+    with open(dataset_path / 'encoded.txt', 'w') as f:
+        # Iterate through the dictionary
+        for k, v in label_dict.items():
+            # Write each key-value pair to the file, with a newline character at the end
+            f.write(f"{v}: {k}\n")
 
 
 def main():
--- dataset-generation/helper.py
+++ dataset-generation/helper.py
@@ -17,7 +17,7 @@ def overlap(boxes1, boxes2):
     return torch.all(inter == 0)
 
 
-def provide_random_coordinates(background, img):
+def provide_random_coordinates(background, img, coordinates, i):
     """ Provides random coordinates for placement of image.
 
     :param background: Background image.
@@ -28,7 +28,25 @@ def provide_random_coordinates(background, img):
     h_background, w_background, _ = background.shape
     y_start = random.randint(5, h_background-h_img-5)
     x_start = random.randint(5, w_background-w_img-5)
-    return [x_start, y_start, x_start+w_img, y_start+h_img]
+
+    co = torch.tensor([x_start, y_start, x_start + w_img,
+                       y_start + h_img]).reshape((1, 4))
+
+    flag = False
+    counter = 0
+    while not overlap(co, coordinates):
+        y_start = random.randint(5, h_background - h_img - 5)
+        x_start = random.randint(5, w_background - w_img - 5)
+        co = torch.tensor([x_start, y_start, x_start + w_img,
+                           y_start + h_img]).reshape((1, 4))
+        counter += 1
+        if counter >= 100:
+            flag = True
+            break
+
+    coordinates[i] = co
+
+    return [x_start, y_start, x_start + w_img, y_start + h_img, flag]
 
 
 def create_bounding_box(mask, x_start, y_start):
@@ -56,40 +74,22 @@ def create_bounding_box(mask, x_start, y_start):
     return x1, y1, x2, y2
 
 
-def create_label_object(x, y, width, height, label):
-    """ Creates label for YOLO algorithm
-
-    :param file:
-    :param x:
-    :param y:
-    :param width:
-    :param height:
-    :return:
-    """
-    label_txt = ''
-    label_txt += str(x) + ','  # x1
-    label_txt += str(y) + ','  # y1
-    label_txt += str(width) + ','  # x2
-    label_txt += str(height) + ','  # y2
-    label_txt += str(label) + ' '
-    return label_txt
-
-def create_label_objects(x1_start, y1_start, labels, min_y, scale):
-    label_txt = ''
+def create_label_objects(x1_start, y1_start, labels, min_y, background_width, background_height):
+    label_list = []
     for bbox in labels:
-        x1 = int(int(bbox[0]) * scale) + x1_start
-        y1 = int((int(bbox[1]) - min_y) * scale) + y1_start
-        x2 = int(int(bbox[2]) * scale) + x1_start + 2
-        y2 = int((int(bbox[3]) - min_y) * scale) + y1_start + 2
+        x1 = int(int(bbox[0]) + x1_start)
+        y1 = int(int(bbox[1]) - min_y + y1_start)
+        x2 = int(int(bbox[2]) + x1_start) + 2
+        y2 = int(int(bbox[3]) - min_y + y1_start) + 2
         label = bbox[4]
-        # plt.gca().add_patch(Rectangle((x1, y1), x2-x1, y2-y1, linewidth=1, edgecolor='r', facecolor='none'))
-        
-        label_txt += str(x1) + ','     # x1
-        label_txt += str(y1) + ','               # y1
-        label_txt += str(x2) + ','     # x2
-        label_txt += str(y2) + ','               # y2
-        label_txt += str(label) + ' '
-    return label_txt
+
+        # yolov7 labels expect normalized x_center, y_center and width and height
+        x = (x1 + x2) / 2 / background_width
+        y = (y1 + y2) / 2 / background_height
+        width = (x2 - x1) / background_width
+        height = (y2 - y1) / background_height
+        label_list.append([label, x, y, width, height])
+    return label_list
 
 
 
@@ -100,7 +100,7 @@ def get_img_mask(img):
     :param img: Input image.
     :return: Mask of image.
     """
-    mask = img.copy_nodes()[:, :, 0]
+    mask = img.copy()[:, :, 0]
     mask[mask > 0] = 1
     return mask
 
@@ -151,6 +151,7 @@ def random_color(image):
     image[:, :, :3] = color
     return image
 
+
 def place_image_on_background(label, image, background, coordinates, i):
     # Prepare and resize background
     background = cv2.cvtColor(background, cv2.COLOR_RGB2RGBA)
@@ -164,15 +165,6 @@ def place_image_on_background(label, image, background, coordinates, i):
     max_y = np.max(nz[0])
     image = image[min_y:max_y]
     img = image
-    scale = 1
-
-    # resize and find coordinates
-    co = torch.tensor(provide_random_coordinates(
-        background, img)).reshape((1, 4))
-    while not overlap(co, coordinates):
-        co = torch.tensor(provide_random_coordinates(
-            background, img)).reshape((1, 4))
-    coordinates[i] = co
 
     mask = get_img_mask(img)
     img[:, :, 3] = mask
@@ -180,7 +172,10 @@ def place_image_on_background(label, image, background, coordinates, i):
     background_alpha = 1.0 - mask
 
     # get placement coordinates
-    x_start, y_start, x_end, y_end = co[0].tolist()
+    x_start, y_start, x_end, y_end, flag = provide_random_coordinates(background, img, coordinates, i)
+
+    if flag:
+        return background, None, flag
 
     # use numpy indexing to place the resized image in the background image
     for c in range(0, 3):
@@ -189,82 +184,10 @@ def place_image_on_background(label, image, background, coordinates, i):
              background[y_start:y_end, x_start:x_end, c])
 
     x1_start, y1_start, _, _ = create_bounding_box(mask, x_start, y_start)
-    label_text = create_label_objects(x1_start, y1_start, label, min_y, scale)
-    # plt.imshow(background)
-    # plt.show()
-    return background, label_text
+    background_height, background_width = background.shape[:2]
+    label_text = create_label_objects(x1_start, y1_start, label, min_y, background_width, background_height)
+    return background, label_text, False
 
-
-
-
-
-
-
-
-
-
-
-###################################################################
-def place_images_on_grid_background2(labels, images, background):
-    # Prepare and resize background
-    background = cv2.cvtColor(background, cv2.COLOR_RGB2RGBA)
-    background = resize(background, 450)
-
-
-    dims_x = int(images[0].shape[0]*1.3) # minimum width of grid cell cause of img shape
-    dims_y = int(images[0].shape[1]*1.3) # minimum height of grid cell cause of img shape
-    # Get grid depending on background image
-    grid = random.randint(1, int(background.shape[0] / dims_x)), \
-           random.randint(1, int(background.shape[1] / dims_y))
-
-    dims_x = int(background.shape[0] / grid[0]) # resize to new dimensions
-    dims_y = int(background.shape[1] / grid[1])
-
-    for i in range(grid[0]):
-        for j in range(grid[1]):
-            rand_int = random.randint(0, len(images)-1)
-            image = swap_black_white(images[rand_int])
-            # resize and find coordinates
-            img = resize(image, random.randint(
-                int(image.shape[0]/2), int(image.shape[0]*1.3)))
-            co = [i * dims_x, j * dims_y, (i * dims_x + img.shape[0]),
-                  (j * dims_y + img.shape[1])]
-            co = torch.tensor(co).reshape((1, 4))
-
-            mask = get_img_mask(img)
-            img[:, :, 3] = mask
-            img = random_color(img)
-            background_alpha = 1.0 - mask
-
-            # get placement coordinates
-            x_start, y_start, x_end, y_end = co[0].tolist()
-
-            # use numpy indexing to place the resized image in the background image
-            for c in range(0, 3):
-                background[x_start:x_end, y_start:y_end, c] = \
-                    (mask * img[:, :, c] + background_alpha *
-                     background[x_start:x_end, y_start:y_end, c])
-
-            x1, y1, x2, y2 = create_bounding_box(mask, x_start, y_start)
-            label_text = create_label_object(x1, y1, x2, y2, labels[rand_int])
-    return background, label_text
-
-
-def place_images_on_grid_background(labels, images, background):
-    # Prepare and resize background
-    background = cv2.cvtColor(background, cv2.COLOR_RGB2RGBA)
-    background = resize(background, 450)
-
-    max_line_height = int(images[0].shape[1] * 1.3)
-
-    # Get max amount of lines
-    max_lines = int(background.shape[0] / max_line_height)
-
-    # Place images on line
-    for i in range(max_lines):
-        image = swap_black_white(images[i])
-
-        # get bounding box of image
 
 
 
