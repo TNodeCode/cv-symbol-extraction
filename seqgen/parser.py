a8f966c1d2fbdc7aef077140bdc7b898cfafebf8
tnodecode <s2.tf@we-code-it.de>
2023-02-05 16:02:02 +0100

    Added Symbol Replacement

--- seqgen/parser.py
+++ seqgen/parser.py
@@ -11,12 +11,15 @@ def parse_label_file(content):
     formulas = []
     boxes = []
     for idx_line in range(len(lines)):
+        assert lines[idx_line].count("$") == 2, f"Line {idx_line}: There need to be exactly two $ symbols in a line"
+        assert lines[idx_line][0] == "$", f"Line {idx_line}: Line The first symbol must be a $ symbol"
+        # Find the position of the second dollar symbol
         end = lines[idx_line][1:].index("$")
         formula = lines[idx_line][1:end+1]
         formulas.append(formula)
         suffix = lines[idx_line][end+3:]
         coords = suffix.split(" ")
-        assert len(coords) % 5 == 0, "There must be 5n items in the coordinates string"
+        assert len(coords) % 5 == 0, f"Line {idx_line}, formula {formula}: There must be 5n items in the coordinates string"
         boxes.append(np.array(coords, dtype=float).reshape(-1, 5))
     return formulas, boxes
 
